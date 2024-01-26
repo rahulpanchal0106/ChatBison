b2ae8b67d1ab23e5c7b42e7b7ba1f0d40a169a63
Rahul Panchal <rm5901960@gmail.com>
2024-01-26 11:37:37 +0530

    Fix: admin panel backend bug fixed

--- controllers/getAdminDash.js
+++ controllers/getAdminDash.js
@@ -5,6 +5,8 @@ async function getAdminLogin(req,res){
         createdAt: {
             $gte: '2023-11-16T07:26:36.769+00:00',
       }});
+
+    console.log(cursor);
     const prompts = [];
     const responses = [];
     const createdAt=[];
@@ -16,15 +18,29 @@ async function getAdminLogin(req,res){
     const lons=[];
 
     await cursor.forEach((doc)=>{
-        const prompt = doc.prompt;
-        const resp = doc.resp;
-        const date = doc.createdAt;
-        const ip = doc.userInfo['IP Address'];
-        const region = doc.userInfo.Region;
-        const city = doc.userInfo.City;
-        const country = doc.userInfo.Country;
-        const lat = doc.userInfo.Latitude;
-        const lon = doc.userInfo.Longitude;
+        const prompt =  doc.prompt;
+        const resp =  doc.resp;
+        const date =  doc.createdAt;
+        const userInfo = doc.userInfo;
+        var ip,region,city,country,lat,lon;
+        if(doc.userInfo){
+            console.log("userInfo doc Is Present 🟢🟢🟢");
+            ip = doc.userInfo["IP Address"];
+            region = doc.userInfo.Region;
+            city = doc.userInfo.City;
+            country = doc.userInfo.Country;
+            lat = doc.userInfo.Latitude;
+            lon = doc.userInfo.Longitude;
+        }else{
+            console.log("userInfo is NOT FOUND 🔴🔴🔴");
+            ip = "NOT FOUND"
+            region = "NOT FOUND";
+            city = "NOT FOUND";
+            country = "NOT FOUND";
+            lat = "NOT FOUND";
+            lon = "NOT FOUND";
+        }
+        
 
         prompts.push(prompt);
         responses.push(resp);
