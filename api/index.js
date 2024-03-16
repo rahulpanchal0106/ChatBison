00ab96aff1cd67fb2781c6ff74e2ff41f5f38f47
Rahul Panchal <rm5901960@gmail.com>
2024-03-16 15:46:08 +0530

    Setting up vercel config

---
+++ api/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+
+const fs=require('fs');
+
+const sendPrompt=require('./controllers/sendPrompt');
+const getIndex=require('./controllers/getIndex');
+const authenticate = require('./controllers/auth');
+const getAdminLogin = require('./controllers/getAdminLogin')
+const getAdminDash = require('./controllers/getAdminDash')
+let prompt;
+const logStream = fs.createWriteStream(path.join(__dirname, 'console.log'), { flags: 'a' });
+console.log = function(message) {
+  process.stdout.write(`${message}\n`);
+  logStream.write(`${new Date().toISOString()} - ${message}\n`);
+};
+    
+app.use(morgan('combined'));
+app.use(express.json());
+app.set('trust proxy', true);
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/', getIndex);
+
+// var messages = []
+
+app.post('/send_prompt',sendPrompt);
+
+app.get('/admin',getAdminLogin)
+app.get('/getAdmin',authenticate,getAdminDash);
+
+module.exports = {
+    app,
+    prompt
+};
