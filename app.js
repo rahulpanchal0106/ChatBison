4f681b74c53026e80797cc54018c611a0fea4d5b
Rahul Panchal <rm5901960@gmail.com>
2024-03-16 16:03:55 +0530

    Setting up vercel config

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-const express = require("express");
+require('dotenv').config();
+
+const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const path = require('path');
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-app.use("/api", require("./api"));
+const fs=require('fs');
 
-module.exports = app;
+const sendPrompt=require('./controllers/sendPrompt');
+const getIndex=require('./controllers/getIndex');
+const authenticate = require('./controllers/auth');
+const getAdminLogin = require('./controllers/getAdminLogin')
+const getAdminDash = require('./controllers/getAdminDash')
+let prompt;
+const logStream = fs.createWriteStream(path.join(__dirname, 'console.log'), { flags: 'a' });
+console.log = function(message) {
+  process.stdout.write(`${message}\n`);
+  logStream.write(`${new Date().toISOString()} - ${message}\n`);
+};
+    
+app.use(morgan('combined'));
+app.use(express.json());
+app.set('trust proxy', true);
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/', getIndex);
+
+// var messages = []
+
+app.post('/send_prompt',sendPrompt);
+
+app.get('/admin',getAdminLogin)
+app.get('/getAdmin',authenticate,getAdminDash);
+
+module.exports = {
+    app,
+    prompt
+};
